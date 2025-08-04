42c6d1ed1b84d9ac9cf955fa6f0fb23089fa8789
Huangxc <935142179@.com>
2025-08-04 14:03:26 +0800

    修改为liunx处理内容

--- run_kq.sh
+++ run_kq.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "开始运行自动化考勤脚本"
 
-# 激活虚拟环境（如果有的话，路径示例）
-source venv/bin/activate
+# 激活虚拟环境
+. venv/bin/activate
 
 # 安装依赖（如果需要）
 # pip install -r requirements.txt
 
 # 执行自动化测试
-python3 test_login.py
+python test_login.py
 
 echo "自动化脚本执行完毕"
